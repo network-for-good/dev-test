f107e56a0801187132e4e44eed2b147ad72a93cb
Thomas Hoen <tom@givecorps.com>
2020-12-18 11:08:23 -0500

    improves organization of the question specs and adds in additional tests to clarify what we would like to see

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -19,32 +19,43 @@ RSpec.describe Question, type: :model do
 
     context "when an Option already exists" do
       before do
-        question.options_string = 'blue'
+        question.options_string = option_name
       end
 
       context 'and it matches one of the new Options' do
+        let(:option_name) { 'blue' }
+
         it 'should add only the new Option' do
-          expect { 
+          expect {
             question.options_string = 'red, blue, green'
             question.reload
           }.to change(question.options, :count).from(1).to(3)
         end
 
-        it 'should change the position of the existing Option' do
+        it 'changes the position of the existing Option' do
           question.options_string = 'red, blue, green'
           question.reload
           expect(question.options.order(:position).pluck(:name)).to eq(%w{red blue green})
         end
+
+        it 'changes the position of the blue Option' do
+          blue_option = question.options.find_by(name: option_name)
+          expect { question.options_string = 'red, blue, green' }.to change { blue_option.reload.position }.from(0).to(1)
+        end
       end
 
       context "and it doesn't match any of the new Options" do
-        before do
-          question.options_string = 'orange'
-        end
+        let(:option_name) { 'orange' }
 
-        it 'the original Option should be removed' do
+        it 'the original Option is removed from the list' do
           question.options_string = 'red, blue, green'
           expect(question.reload.options.map(&:name)).not_to include("orange")
+        end
+
+        it 'deletes the orange option' do
+          orange_option = question.options.find_by(name: option_name)
+          question.options_string = 'red, blue, green'
+          expect(Option.find_by(id: orange_option.id)).to be_nil
         end
       end
     end
